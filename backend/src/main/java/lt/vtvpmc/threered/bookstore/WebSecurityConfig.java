dc3680c21e8a780f7370547ba21dc58f4b34acf4
Rasa <fiammara@gmail.com>
2018-03-09 20:51:05 +0200

    secure login

--- backend/src/main/java/lt/vtvpmc/threered/bookstore/WebSecurityConfig.java
+++ backend/src/main/java/lt/vtvpmc/threered/bookstore/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package lt.vtvpmc.threered.bookstore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,7 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
     private UserDetailsService userDetailsService;
-
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	   @Override
+	   public AuthenticationManager authenticationManagerBean() throws Exception {
+	       return super.authenticationManagerBean();
+	   }
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
