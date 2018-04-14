2378bb5c59900a8d64ffac1df20c2528f9cb97ad
Ernesta Duglas <duglas.ernesta@gmail.com>
2018-04-15 01:44:03 +0300

    general design modified

--- frontend/src/AdminMenu.js
+++ frontend/src/AdminMenu.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import { NavLink } from 'react-router-dom';
+import Navbar from 'react-bootstrap/lib/Navbar';
 
 const AdminMenu = () => (
-    <nav className='navSide'>
-      <ul>
-        <li><NavLink to='/adminmenu/addAdmin'>Add new administrator</NavLink></li>
-        <li><NavLink to='/adminmenu/addSeller'>Add new seller</NavLink></li>
-        <li><NavLink to='/adminmenu/addBook'>Add new book</NavLink></li>
-        <li><NavLink to='/adminmenu/allBooks'>Visos knygos</NavLink></li>
-      </ul>
-    </nav>
-  );
+  <Navbar inverse collapseOnSelect className="Navbar">
+      <div className="navmenu">
+        <ul className="mainmenu">
+          <li><NavLink to='/adminmenu/allBooks'><i className="fa fa-globe menu-icon" aria-hidden="true"></i>Visos knygos</NavLink></li>
+          <li><NavLink to='/adminmenu/addAdmin'><i className="fa fa-th-list menu-icon" aria-hidden="true"></i>Pridėti administratorių</NavLink></li>
+          <li><NavLink to='/adminmenu/addSeller'><i className="fa fa-th-list menu-icon" aria-hidden="true"></i>Pridėti pardavėją</NavLink></li>
+          <li><NavLink to='/adminmenu/addBook'><i className="fa fa-th-list menu-icon" aria-hidden="true"></i>Pridėti knygą</NavLink></li>
+        </ul>
+      </div>
+  </Navbar>
+);
 
-  export default AdminMenu;
+export default AdminMenu;
